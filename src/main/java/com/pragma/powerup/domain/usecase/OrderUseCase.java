2d94a2f8e2a223314aca4a2afa8e981135511262
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2023-11-24 15:28:52 -0500

    feat: validate that dishes from order belongs to order's restaurant

--- src/main/java/com/pragma/powerup/domain/usecase/OrderUseCase.java
+++ src/main/java/com/pragma/powerup/domain/usecase/OrderUseCase.java
@@ -1,18 +1,30 @@
 package com.pragma.powerup.domain.usecase;
 
 import com.pragma.powerup.domain.api.IOrderServicePort;
+import com.pragma.powerup.domain.exception.DomainException;
 import com.pragma.powerup.domain.model.OrderModel;
+import com.pragma.powerup.domain.spi.IDishPersistencePort;
 import com.pragma.powerup.domain.spi.IOrderPersistencePort;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 public class OrderUseCase implements IOrderServicePort {
     private final IOrderPersistencePort orderPersistencePort;
+    private final IDishPersistencePort dishPersistencePort;
 
     @Override
     public OrderModel saveModel(OrderModel orderModel) {
+        orderModel.getDishQtyList().forEach(orderDishQtyEntity -> {
+            if(!Objects.equals(
+                    dishPersistencePort.getDishById(orderDishQtyEntity.getDish().getId()).getRestaurant().getId(),
+                    orderModel.getRestaurant().getId())
+            ){
+                throw new DomainException("At least one dish does not belong to restaurant");
+            }
+        });
         return orderPersistencePort.saveOrder(orderModel);
     }
     @Override
